01c43fee166740d4ce6220948c759dd5c4efae37
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2024-10-11 21:01:53 +0300

    fix ProductSerializer create method

--- bulk_insert_api/product_api/serializers.py
+++ bulk_insert_api/product_api/serializers.py
@@ -1,12 +1,11 @@
 from rest_framework import serializers
 from .models import Product, ProductVariant
-from django.db import transaction
 
 
 class ProductVariantSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProductVariant
-        fields = "__all__"
+        fields = ["id", "sku", "name", "price"]
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -17,26 +16,11 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = ["id", "name", "image", "variants"]
 
     def create(self, validated_data):
-        variants_data_list = []
-        products_data = []
-
-        for product_data in validated_data:
-            variants_data = product_data.pop("variants")
-            product = Product(**product_data)
-            products_data.append(product)
-
-            for variant_data in variants_data:
-                variant_data["product_id"] = product
-                variants_data_list.append(variant_data)
-
-        with transaction.atomic():
-            products = Product.objects.bulk_create(products_data)
-
-            variants = [
-                ProductVariant(product=product, **variant_data)
-                for product, variant_data in zip(products, variants_data_list)
-            ]
-
-            ProductVariant.objects.bulk_create(variants)
-
-        return products
+        variants_data = validated_data.pop("variants")
+        product = Product.objects.create(**validated_data)
+        variants = [
+            ProductVariant(product_id=product, **variant_data)
+            for variant_data in variants_data
+        ]
+        ProductVariant.objects.bulk_create(variants)
+        return product
